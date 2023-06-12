601346c2f9c7319d264b05ce64aef656740f8f40
Kulikbass <99739561+Kulikbass@users.noreply.github.com>
2023-06-12 14:25:36 +0300

    Update Main.java

--- src/main/java/ru/maximumtest/Main.java
+++ src/main/java/ru/maximumtest/Main.java
@@ -2,9 +2,6 @@ package ru.maximumtest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!121" +
-                "" +
-                "" +
-                "asdasdasdasd212");
+        System.out.println("Hello Agryz");
     }
 }
