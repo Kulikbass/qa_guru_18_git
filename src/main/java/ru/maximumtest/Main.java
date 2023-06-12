bdb4fc0b4815e252f0ec0205ca83be3535f53305
vladislavkulakov <kulikbass@gmail.com>
2023-06-12 14:14:46 +0300

    .gitignore update

---
+++ src/main/java/ru/maximumtest/Main.java
@@ -0,0 +1,10 @@
+package ru.maximumtest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!121" +
+                "" +
+                "" +
+                "asdasdasdasd212");
+    }
+}
